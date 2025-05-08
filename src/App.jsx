6e3ab19731b06f3a6cf9c77856f82c862714b417
ricardo <ricardo03cdof@objectdata.com.br>
2025-05-08 00:52:08 -0300

    tailwind e daisy ui configurados

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <h1>Hello World!</h1>
+        <h1 className='text-red-500'>Hello World!</h1>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
@@ -29,6 +29,18 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      {/* You can open the modal using document.getElementById('ID').showModal() method */}
+<button className="btn" onClick={()=>document.getElementById('my_modal_3').showModal()}>open modal</button>
+<dialog id="my_modal_3" className="modal">
+  <div className="modal-box">
+    <form method="dialog">
+      {/* if there is a button in form, it will close the modal */}
+      <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+    </form>
+    <h3 className="font-bold text-lg">Hello!</h3>
+    <p className="py-4">Press ESC key or click on ✕ button to close</p>
+  </div>
+</dialog>
     </>
   )
 }
